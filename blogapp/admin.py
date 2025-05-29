77f69e34e4642b8189a95c3ad6b7fa55ad07860c
= <=>
2025-05-29 12:52:32 +0100

    add: models Articles

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# models
+from .models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('author', 'created_at')
+
+
+admin.site.register(Article, ArticleAdmin)
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# models
+
+
+# article model
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(
+        to=User, 
+        on_delete=models.PROTECT,
+        related_name='articles',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
