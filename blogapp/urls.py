d96a4fa358ecacf0a1cc03ae20058b9281733e3f
sabbat10 <sabbatlumpatshia99@gmail.com>
2025-05-31 14:43:47 +0100

    add: logout

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blogapp.views import home_view, connexion_view, inscription_view
+from blogapp.views import home_view, connexion_view, inscription_view, deconnexion_view
 
  
 app_name = 'blogapp'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('connexion/', connexion_view, name='connexion'),
     path('inscription/', inscription_view, name='inscription'),
+    path('deconnexion/', deconnexion_view, name='deconnexion'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 
 # views.py
 
-
+# home view
 def home_view(request):
     return render(request, 'home.html')
 
@@ -13,6 +13,7 @@ def home_view(request):
 def connexion_view(request):
     return render(request, 'connexion.html')
 
+# inscription view
 def inscription_view(request):
     
     if request.method == 'POST':
@@ -25,3 +26,9 @@ def inscription_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'inscription.html', {'form': form})
+
+
+# logout view
+def deconnexion_view(request):
+    logout(request)
+    return redirect('blogapp:home')
