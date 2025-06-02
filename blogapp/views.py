d2805823ece5eccd02bd95b0fc782852479f9aa3
sabbat10 <sabbatlumpatshia99@gmail.com>
2025-06-02 13:39:12 +0100

    add: content login

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 
 
@@ -11,7 +11,15 @@ def home_view(request):
 
 
 def connexion_view(request):
-    return render(request, 'connexion.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('blogapp:home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'connexion.html', {'form': form})
 
 # inscription view
 def inscription_view(request):
